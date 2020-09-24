2b6e68ca5312d9c9ecad18d5d01fc84352692182
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-09-25 00:20:46 +0900

    structopt

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 use std::fs;
-use std::env;
 use dirs::home_dir;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    #[structopt(parse(from_os_str))]
+    filename: std::path::PathBuf,
+}
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let args = Cli::from_args();
+    let filename = args.filename;
     let home = home_dir().unwrap();
     let dotfiles = home.join("dotfiles");
-    let src = dotfiles.join(filename);
-    let dst = home.join(filename);
+    let src = dotfiles.join(&filename);
+    let dst = home.join(&filename);
     fs::hard_link(src, dst).unwrap();
 }
