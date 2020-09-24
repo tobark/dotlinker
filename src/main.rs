27ae0cbdbd047f77a5e1c13abffae18dc92962ed
Yi Hyunjoon <yi.hyunjoon@gmail.com>
2020-09-24 23:55:27 +0900

    core

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs;
+use std::env;
+use dirs::home_dir;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let home = home_dir().unwrap();
+    let dotfiles = home.join("dotfiles");
+    let src = dotfiles.join(filename);
+    let dst = home.join(filename);
+    fs::hard_link(src, dst).unwrap();
 }
